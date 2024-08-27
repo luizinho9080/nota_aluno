d92ec29e38161ed10a82c4f327cfa18ed058df9a
luizinho9080 <127403372+luizinho9080@users.noreply.github.com>
2024-08-27 09:19:56 -0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,14 @@
+const nota_1 = 10
+const nota_2 = 5
+const nota_3 = 4
+
+const soma_notas = nota_1 + nota_2 + nota_3
+const media = soma_notas / 3
+
+if (media>= 6) {
+    console. log (o aluno foi aprovado)
+}
+se media>= 4 && media <6, console log ('está de prova final')
+else{
+console. log ('o aluno foi reprovado')     
+}
